e044bdd758cfa790a6f1251b451c8c7555d26924
uapv2401103 <uapv2401103@e-mc135-02.ad.univ-avignon.fr>
2026-04-28 18:53:52 +0200

    ajout de la fonction display

--- compilateur.cpp
+++ compilateur.cpp
@@ -31,7 +31,7 @@ using namespace std;
 enum OPREL {EQU, DIFF, INF, SUP, INFE, SUPE, WTFR};
 enum OPADD {ADD, SUB, OR, WTFA};
 enum OPMUL {MUL, DIV, MOD, AND ,WTFM};
-enum TYPES {UNSIGNED_INT, BOOLEAN};
+enum TYPES {INTEGER, BOOLEAN};
 
 TOKEN current;				// Current token
 
@@ -59,14 +59,14 @@ void Error(string s){
 enum TYPES Identifier(void){
 	cout << "\tpush "<<lexer->YYText()<<endl;
 	current=(TOKEN) lexer->yylex();
-	return UNSIGNED_INT;
+	return INTEGER;
 }
 
 // Number := Digit{Digit}
 enum TYPES Number(void){
 	cout <<"\tpush $"<<atoi(lexer->YYText())<<endl;
 	current=(TOKEN) lexer->yylex();
-	return UNSIGNED_INT;
+	return INTEGER;
 }
 
 enum TYPES Expression(void);			// Called by Term() and calls Term()
@@ -294,7 +294,7 @@ void AssignementStatement(void){
 		cerr << "Erreur : Variable '"<<lexer->YYText()<<"' non déclarée"<<endl;
 		exit(-1);
 	}
-	type1 = UNSIGNED_INT;
+	type1 = INTEGER;
 	variable=lexer->YYText();
 	current=(TOKEN) lexer->yylex();
 	if(current!=ASSIGN)
@@ -393,7 +393,28 @@ void IfStatement(void){
 	}
 }
 
-//Statement := AssignementStatement | IfStatement | WhileStatement | ForStatement | BlockStatement
+//  DisplayStatement := "DISPLAY" expression           Affiche expression
+void DisplayStatement(void){
+	enum TYPES type;
+	if (current == DISPLAY){
+		current=(TOKEN) lexer->yylex();
+		type = Expression();
+		if (type != INTEGER){
+			Error("Expression de type interger attendu");
+		}
+		cout << "\tpop %rdx\t# The value to be displayed"<<endl;
+		cout << "\tmovq $FormatString1, %rsi\t# \"%llu\\n\""<<endl;
+		cout << "\tmovl	$1, %edi"<<endl;
+		cout << "\tmovl	$0, %eax"<<endl;
+		cout << "\tcall	__printf_chk@PLT"<<endl;
+
+	}else{
+		Error("Display attendu");
+	}
+
+}
+
+//Statement := AssignementStatement | IfStatement | WhileStatement | ForStatement | BlockStatement | DisplayStatement
 void Statement(void){
 	if (current == ID){
 		AssignementStatement();
@@ -405,6 +426,8 @@ void Statement(void){
 		ForStatement();
 	}else if (current == BEG){
 		BlockStatement();
+	}else if (current == DISPLAY){
+		DisplayStatement();
 	}else{
 		Error("Absence de mot clé");
 	}
@@ -436,6 +459,8 @@ void Program(void){
 int main(void){	// First version : Source code on standard input and assembly code on standard output
 	// Header for gcc assembler / linker
 	cout << "\t\t\t# This code was produced by the CERI Compiler"<<endl;
+	cout << "\t.section .rodata"<<endl;
+	cout << "FormatString1:\t.string \"%llu\\n\""<<endl;
 	// Let's proceed to the analysis and code production
 	current=(TOKEN) lexer->yylex();
 	Program();
